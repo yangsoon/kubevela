300a2e05e9ac3bb287709e035d598873d98eaf43
yangsoon <yangsoonlx@gmail.com>
2021-04-18 11:42:22 +0800

    test local

--- hack/apis/sync.sh
+++ hack/apis/sync.sh
@@ -28,7 +28,7 @@ echo "git clone"
 cd ..
 git config --global user.email "kubevela.bot@aliyun.com"
 git config --global user.name "kubevela-bot"
-git clone --single-branch --depth 1 git@github.com:oam-dev/kubevela-core-api.git kubevela-core-api
+git clone --single-branch --depth 1 git@github.com:yangsoon/kubevela-core-api.git kubevela-core-api
 
 echo "clear kubevela-core-api api/"
 rm -r kubevela-core-api/apis/*
@@ -56,5 +56,5 @@ then
 else
   git add .
   git commit -m "sync commit $COMMIT_ID from kubevela-$VERSION"
-  git push origin main
+  git push origin master
 fi
